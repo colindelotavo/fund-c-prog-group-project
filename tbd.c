4e42953de85ed8385f2f89baa3b81fdcb36ffc5b
colindelotavo <60180147+colindelotavo@users.noreply.github.com>
2024-04-15 15:30:23 +1000

    Create tbd.c

---
+++ tbd.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+ * 48430 Fundamentals of C Programming - Assignment 3
+
+ * Group Number: 56
+
+ * Lab Number: 04
+
+ * Group Members: Colin Tria Delotavo and Samarth Kunal Singh
+ 
+ * Date of submission: dd/05/2024
+
+******************************************************************************/
+
+/******************************************************************************
+
+ * List header files - do NOT use any other header files. Note that stdlib.h is
+   included in case you want to use any of the functions in there. However the
+   task can be achieved with stdio.h and string.h only.
+
+******************************************************************************/
+
+/******************************************************************************
+
+ * List preprocessing directives - you may define your own.
+ * Each defined with max size + null terminator.
+
+******************************************************************************/
+
+/******************************************************************************
+
+ * Function prototypes - do NOT change the given prototypes. However you may
+   define your own functions if required.
+
+******************************************************************************/
