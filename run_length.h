a71ea5bfd25a661daf3be6485ad72bbb4c7b5370
colindelotavo <60180147+colindelotavo@users.noreply.github.com>
2024-04-22 15:42:41 +1000

    Update run_length.h

--- run_length.h
+++ run_length.h
@@ -1,1 +1,10 @@
+#ifndef RUN_LENGTH_H_   /* Include guard */
+#define RUN_LENGTH_H_
 
+void run_length_compress(const char input_filename[], 
+                         const char output_filename[]);
+
+void run_length_decompress(const char input_filename[], 
+                           const char output_filename[]);
+
+#endif // RUN_LENGTH_H_
