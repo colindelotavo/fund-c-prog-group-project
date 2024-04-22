176cc8a525a234eafd70ccc5b944380f82d03100
colindelotavo <60180147+colindelotavo@users.noreply.github.com>
2024-04-22 15:45:35 +1000

    Update caeser_cipher.c

--- caeser_cipher.c
+++ caeser_cipher.c
@@ -1,1 +1,12 @@
+#include "caeser_cipher.h"
 
+/* defining functions here */
+void caeser_cipher_encrypt(const char input_filename[], 
+                           const char output_filename[], int shift_key) {
+
+}
+
+void caeser_cipher_decrypt(const char input_filename[], 
+                           const char output_filename[], int shift_key) {
+
+}
