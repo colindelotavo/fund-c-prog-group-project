441099f601814d0c039ac742b5878dfb8cbaaf84
colindelotavo <60180147+colindelotavo@users.noreply.github.com>
2024-04-22 15:46:57 +1000

    Update run_length.c

--- run_length.c
+++ run_length.c
@@ -1,1 +1,12 @@
+#define "run_length.h"
 
+/* defining functions here */
+void run_length_compress(const char input_filename[], 
+                         const char output_filename[]) {
+
+}
+
+void run_length_decompress(const char input_filename[], 
+                           const char output_filename[]) {
+
+}
