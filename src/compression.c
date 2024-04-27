752d40d34279ff2fd7a527c1110cef7f17f18d03
Colin Delotavo <colin.delotavo@gmail.com>
2024-04-27 13:45:43 +1000

    Clean folders

---
+++ src/compression.c
@@ -0,0 +1,39 @@
+#include "compression.h"
+
+/******************************************************************************
+
+ * What function does
+
+ * inputs:
+   - a
+   - b
+
+ * outputs:
+   - y
+   - z
+
+******************************************************************************/
+
+void run_length_compress(const char input_filename[], 
+                         const char output_filename[]) {
+
+}
+
+/******************************************************************************
+
+ * What function does
+
+ * inputs:
+   - a
+   - b
+
+ * outputs:
+   - y
+   - z
+
+******************************************************************************/
+
+void run_length_decompress(const char input_filename[], 
+                           const char output_filename[]) {
+
+}
