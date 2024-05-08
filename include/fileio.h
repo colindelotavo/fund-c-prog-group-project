23a49ac53a25d77acf8977032ec8f12916ff291e
Colin Delotavo <colin.delotavo@gmail.com>
2024-05-08 22:25:50 +1000

    Rename from display_file to fileio

---
+++ include/fileio.h
@@ -0,0 +1,13 @@
+#ifndef FILEIO_H_   /* Include guard */
+#define FILEIO_H_
+
+/******************************************************************************
+
+
+
+******************************************************************************/
+
+void display_file(const char input_filename[], 
+                  const char output_filename[]);
+
+#endif 
