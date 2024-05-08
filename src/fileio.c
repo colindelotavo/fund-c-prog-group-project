b3816c40a7a60f36a3ac9359290693bd8f835ec1
Colin Delotavo <colin.delotavo@gmail.com>
2024-05-08 22:25:29 +1000

    Rename from display_file to fileio

--- src/fileio.c
+++ src/fileio.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "../include/display_file.h"
+#include "../include/fileio.h"
 
 /******************************************************************************
 
