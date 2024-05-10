da979a6d8e7654ad9d1afaac79d3697a7fa5a482
Colin Delotavo <colin.delotavo@gmail.com>
2024-05-10 22:44:10 +1000

    User input function

--- src/main.c
+++ src/main.c
@@ -66,21 +66,21 @@ int main(void) {
         print_menu();
         scanf(" %d", &choice); getchar();
         if (choice == 1) {
-            // user_input(INPUT_FILE);
+            user_input(INPUT_FILE);
             compress_file(INPUT_FILE, COMPRESSED_FILE);
             display_file(INPUT_FILE, COMPRESSED_FILE);
         } else if (choice == 2) {
-            // user_input(INPUT_FILE);
+            user_input(INPUT_FILE);
             decompress_file(COMPRESSED_FILE, DECOMPRESSED_FILE);
             display_file(COMPRESSED_FILE, DECOMPRESSED_FILE);
         } else if (choice == 3) {
-            // user_input(INPUT_FILE);
+            user_input(INPUT_FILE);
             printf("Enter shift key for encryption> ");
             scanf(" %d", &shift_key);
             encrypt_file(INPUT_FILE, ENCRYPTED_FILE, shift_key);
             display_file(INPUT_FILE, ENCRYPTED_FILE);
         } else if (choice == 4) {
-            // user_input(INPUT_FILE);
+            user_input(INPUT_FILE);
             printf("Enter shift key for decryption> ");
             scanf(" %d", &shift_key);
             decrypt_file(ENCRYPTED_FILE, DECRYPTED_FILE, shift_key);
