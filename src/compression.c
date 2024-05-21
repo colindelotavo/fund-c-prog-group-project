8f2dc32578fd3046cc8b45483156d639562bdc96
Colin Delotavo <colin.delotavo@gmail.com>
2024-05-21 22:15:34 +1000

    Update compression.c

--- src/compression.c
+++ src/compression.c
@@ -1,31 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
+#include <stdio.h> /* fopen, fclose, fgetc, fputc, fprintf, fscanf, printf */
 #include "../include/compression.h"
 
 /******************************************************************************
 
- * What function does (e.g. using run-length encoding etc)
+ * Primary function for compression to enable run-length encoding based on
+   the input_file (which contains user input)
 
  * inputs:
-   -
+   - input_file text contents
 
  * outputs:
-   - 
-
-steps to run as a test:
-
-cd src
-
-gcc -o ../task.out main.c compression.c encryption.c fileio.c -I../include
-
-../task.out
-
- * edge cases to handle
-   - empty line just writes as '1' in output file
-   - what if RLE is more space, ie. string does not use repeated chars
-   - should it only consider uppercase/lowercase..? or keep as is
+   - output_file text compressed/transformed into run-length encoded data
 
 ******************************************************************************/
 
@@ -78,18 +63,16 @@ void compress_file(const char input_filename[],
 
 /******************************************************************************
 
- * What function does (e.g. using run-length encoding etc)
+ * Primary function for decompression to reverse run-length encoding based on
+   the input_file (which is the compressed text)
 
  * inputs:
-   - 
+   - input_file contains compressed file contents
 
  * outputs:
-   - 
-
-steps to run as a test:
-cd src
-gcc -o ../task.out main.c compression.c -I../include
-../task.out
+   - output_file contains reversed RLE data, in other words, the user
+     input that was previously inputted when the compressed option
+     was selected
 
 ******************************************************************************/
 
@@ -132,50 +115,63 @@ void decompress_file(const char input_filename[],
 
 /******************************************************************************
 
- * What function does (e.g. using run-length encoding etc)
+ * Primary function to test if compression was successful, comparing the
+   string length of the input versus output, which is used in the main program
+   as the compressed versus decompressed file contents, respectively
 
  * inputs:
-   -
+   - input_file and output_file text data
 
  * outputs:
-   - 
+   - helpful console print message to confirm whether compression of user input
+     was successful 
 
 ******************************************************************************/
 
 void compare_file_size(const char input_filename[],
-                          const char output_filename[]) {
+                       const char output_filename[]) {
 
+    /* attempt to open the input file in binary mode */
     FILE *input_file_ptr = fopen(input_filename, "rb");
+    /* attempt to open the output file in binary mode */
     FILE *output_file_ptr = fopen(output_filename, "rb");
 
+    /* check if the input file opened successfully */
     if (!input_file_ptr) {
         fprintf(stderr, "Error opening input file: %s\n", input_filename);
         return;
     }
+    /* check if the output file opened successfully */
     if (!output_file_ptr) {
         fprintf(stderr, "Error opening output file: %s\n", output_filename);
         fclose(input_file_ptr);
         return;
     }
 
+    /* variables to store the length of input and output files */
     int input_length = 0;
     int output_length = 0;
     char ch;
 
+    /* calculate the length of the input file */
     while ((ch = fgetc(input_file_ptr)) != EOF) {
         input_length++;
     }
 
+    /* calculate the length of the output file */ 
     while ((ch = fgetc(output_file_ptr)) != EOF) {
         output_length++;
     }
 
+    /* compare the lengths and print the result */
     if (output_length <= input_length) {
         printf("\n\n\nCompression is successful.\n");
     } else {
         printf("\n\n\nCompression has failed.\n");
     }
 
+    /* close the input and output files */
     fclose(input_file_ptr);
     fclose(output_file_ptr);
 }
+
